07553f5f8e931d63894f684a1d7c8c60742be7b5
caipira113 <caipira@libnare.net>
2024-08-19 19:45:16 +0900

    menuDisplay: sideIcon

--- packages/frontend/src/store.ts
+++ packages/frontend/src/store.ts
@@ -320,7 +320,7 @@ export const defaultStore = markRaw(new Storage('base', {
 	},
 	menuDisplay: {
 		where: 'device',
-		default: 'sideFull' as 'sideFull' | 'sideIcon' | 'top',
+		default: 'sideIcon' as 'sideFull' | 'sideIcon' | 'top',
 	},
 	reportError: {
 		where: 'device',
